e602e814faf1e2bb691b4489a98afc1478a75bb6
Anton Afanasiev <anton.afanasyev93@yandex.ru>
2020-10-18 18:07:13 +0300

    add auth.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import auth from "../utils/auth";
 
 function Login() {
   return (
---
+++ src/utils/auth.js
@@ -0,0 +1,52 @@
+import {apiAuthConfig} from "./constants";
+
+class Auth {
+  constructor(authConfig) {
+    this.url = authConfig.baseUrl
+    this.headers = authConfig.headers
+  }
+
+  _resultHandler(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Ошибка: ${res.status}`)
+  }
+
+  register(email, password) {
+    return fetch(`${this.url}/signup`,
+      {
+        method: 'POST',
+        headers: this.headers,
+        body: JSON.stringify({password, email})
+      })
+      .then(this._resultHandler)
+  }
+
+  authorize(email, password) {
+    return fetch(`${this.url}/signin`,
+      {
+        method: 'POST',
+        headers: this.headers,
+        body: JSON.stringify({password, email})
+      })
+      .then(this._resultHandler)
+  }
+
+  getContent(token) {
+    return fetch(`${this.url}/users/me`,
+      {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`,
+        }
+      })
+      .then(this._resultHandler)
+  }
+}
+
+const auth = new Auth(apiAuthConfig);
+
+export default auth;
